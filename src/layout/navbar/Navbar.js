67d15edb2f313746a4d8c945219510654e3c0392
JORDAN MANLEY <jordanmanley@JORDANs-MacBook-Pro.local>
2023-01-19 14:03:10 -0500

    complete responsive navbar"

--- src/layout/navbar/Navbar.js
+++ src/layout/navbar/Navbar.js
@@ -1,23 +1,36 @@
-import React from 'react'
+import React, {useState} from 'react'
 
 import './Navbar.scss';
 
 const Navbar = () => {
-  return (
-    <div className="navbar">
-        <div className="navbar__container">
-            <div className="navbar__logo">Student App</div>
-            <ul className="navbar__items">
-                <li>Students</li>
-                <li>About </li>
-                <li>Contact</li>
-            </ul>
-            <div className="navbar__hamburgerMenu">
-            =
+
+    const [mobileLinksDisplayed, setMobilLinksDisplayed] = useState(false);
+
+    const toggleNavBar = () => {
+        setMobilLinksDisplayed(!mobileLinksDisplayed);
+    }
+
+    return (
+        <div className="navbar">
+            <div className="navbar__container">
+                <div className="navbar__logo">Student App</div>
+                <ul className={mobileLinksDisplayed ? 'navbar__items navbar__items-active' : 'navbar__items'}>
+                    <li>Students</li>
+                    <li>About </li>
+                    <li>Contact</li>
+                </ul>
+                <div 
+                    className="navbar__hamburgerMenu"
+                    onClick={toggleNavBar}
+                >
+                =
+                </div>
             </div>
-        </div>
-        <div className="navbar__overlay"></div>
-    </div>  
+            <div 
+                className={mobileLinksDisplayed ? 'navbar__overlay navbar__overlay-active' : 'navbar__overlay'}
+                onClick={(toggleNavBar)}
+            ></div>
+        </div>  
     )
 }
 
